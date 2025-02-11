d8009b66bfa99dac2efea1521daa2db4078c83c8
Muhammad Haroon <mharoon1326@gmail.com>
2025-02-11 11:53:04 +0500

    `Added gorilla/websocket dependency and implemented basic WebSocket handling`

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,1 +1,34 @@
 package websocket
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func HandleConnections(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	clients[conn] = true
+
+	for {
+		var msg string
+		if err := conn.ReadJSON(&msg); err != nil {
+			delete(clients, conn)
+			break
+		}
+		for client := range clients {
+			client.WriteJSON(msg)
+		}
+	}
+}
