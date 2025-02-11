06048df9cdb8dff6454d044115f6af4556bb6956
Muhammad Haroon <mharoon1326@gmail.com>
2025-02-11 11:45:01 +0500

    `Added jwt middleware and dependencies`

--- middleware/jwt_middleware.go
+++ middleware/jwt_middleware.go
@@ -1,1 +1,18 @@
 package middleware
+
+import (
+	"livechat-support/config"
+	"livechat-support/models"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(user models.User) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, _ := token.SignedString([]byte(config.JwtSecret))
+	return tokenString
+}
